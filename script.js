5cea93c48b7e1ec9345b7ec09a332366f1e7f99e
jjmoonisboss <35401861+jjmoonisboss@users.noreply.github.com>
2019-05-09 08:42:35 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,41 @@
+$('button').click(function(){
+    var input = $('#emote').val();
+    if(input === "happy"){
+        happyFace();
+    } else if(input==="sad"){
+    
+        sadFace();
+    }else if(input==="angry"){
+      angryFace();
+    }else{
+     alert("Please enter sad,happy or angry!");   
+    }
+});
+
+function changeBackground(color){
+    $('body').css("background-color", color);
+}
+
+function happyFace(){
+   changeBackground("yellow");
+    $(".sad").hide();
+    $(".angry").hide();
+    $(".happy").show();
+    
+}
+
+function sadFace(){
+    changeBackground("blue");
+    $(".angry").hide();
+    $(".happy").hide();
+    $(".sad").show();
+    
+}
+
+function angryFace(){
+    changeBackground("red");
+    $(".sad").hide();
+    $(".happy").hide();
+    $(".angry").show();
+    
+}
